f6ace3ea7d3b3603c9fca734903a51e840a8d7ae
Sadhika Kamchetty <145074124+kamchettysadhika@users.noreply.github.com>
2026-04-14 13:54:17 -0400

    Add: valid-tree - submission-7

---
+++ Data Structures & Algorithms/valid-tree/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) > (n - 1):
+            return False
+        # build adjacency list 
+        adj = [[] for i in range(n)]
+        for u,v in edges:
+            # because its undirected 
+            adj[u].append(v)
+            adj[v].append(u)
+        visit = set()
+        # cycle detection 
+        def dfs(node,parent):
+            if node in visit:
+                return False 
+            visit.add(node)
+            for nei in adj[node]:
+                if nei == parent:
+                    continue 
+                
+                if not dfs(nei,node):
+                    return False 
+            return True 
+
+        return dfs(0,-1) and len(visit) == n
+
+
