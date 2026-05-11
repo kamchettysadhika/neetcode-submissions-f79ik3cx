6bb128a5aa52ec80c716aa42ff564025c1fd8132
Sadhika Kamchetty <145074124+kamchettysadhika@users.noreply.github.com>
2026-05-11 12:51:15 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # "Was it a car or a cat I saw?"
+        start,end = 0,len(s)-1
+        while start <end:
+            while start< end and not self.alphaNum(s[start]):
+                start+=1 
+            while start<end and not self.alphaNum(s[end]):
+                end-=1 
+            if s[start].lower() != s[end].lower():
+                    return False 
+            start,end = start+1, end-1
+        return True
+
+    def alphaNum(self,c):
+        return (ord('a') <= ord(c) <= ord('z')  
+        or ord('A') <= ord(c) <= ord('Z') 
+        or  ord('0') <= ord(c) <= ord('9'))
+
+
+           
+
